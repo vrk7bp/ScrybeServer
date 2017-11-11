bcdd7d539657d7ea2b0f6de96893a9dfbdd2532e
gautam <gk@person.clarke.ai>
2017-11-11 14:19:31 -0500

    Adding environment variable check for database

--- Scrybe/Scrybe/settings/production.py
+++ Scrybe/Scrybe/settings/production.py
@@ -1,4 +1,15 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = [‘app.scrybe.com’, ]
+ALLOWED_HOSTS = ['app.scrybe.com', ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PROD_DATABASE_NAME', ''),
+        'USER': os.environ.get('PROD_DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('PROD_DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('PROD_DATABASE_HOST', ''),
+        'PORT': os.environ.get('PROD_DATABASE_PORT', ''),
+    }
+}
